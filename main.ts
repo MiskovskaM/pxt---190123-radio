39e6db50a52bde410a34092a4d6357f6c91e37a3
MiskovskaM <115079420+MiskovskaM@users.noreply.github.com>
2023-01-19 11:37:00 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,29 @@
-basic.forever(function () {
-	
+radio.setTransmitPower(4);
+radio.setFrequencyBand(7);
+
+radio.setTransmitSerialNumber(true);
+radio.setGroup(1)
+const mySerial = control.deviceSerialNumber()
+console.logValue("serial:", mySerial + "\n\r")
+
+let actualCode = 7;
+
+
+
+radio.onReceivedNumber((receivedNumber: number) => {
+    const remoteSerial = radio.receivedPacket(RadioPacketProperty.SignalStrength)
+    console.logValue("BaconId" + remoteSerial, receivedNumber + "\n\r")
+    basic.showNumber(actualCode)
+})
+
+radio.onReceivedValue(function (key: string, value: number) {
+    if (mySerial === parseInt(key)) {
+        console.logValue(key, value)
+        radio.sendNumber(56)
+    }
+    if (key === "grp") {
+        console.logValue("newgroup", value)
+    }
+    console.log("\n\r")
+
 })
